10525eeae03ed825ea5e167a38c7e3c6109459e1
lms <misheng.li@chentuobang.com>
2018-06-20 18:20:01 +0800

    init

---
+++ learn-happyfri/src/page/home/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="home-container">
+		<header class="tool-tips">
+			<span class="num-tips">{{level}}</span>
+		</header>
+		<div class="home-logo"></div>
+		<router-link to="item" class="start button"></router-link>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'home',
+		computed: {
+			level() {
+				return this.$store.state.level;
+			}
+		}
+	}
+</script>
+<style>
+	.home-container {
+		text-align: center;
+		padding-top: 192px;
+	}
+	.home-logo {
+		background-image: url("~@/images/1-2.png");
+		background-size: 100%;
+		width: 616px;
+		height: 544px;
+		margin: 0 auto;
+	}
+	.num-tips {
+		font-size: 28px;/*px*/
+	}
+	.start {
+		background-image: url("~@/images/1-4.png");
+	}
+</style>
---
+++ learn-happyfri/src/store/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+const state = {level:'第一周'};
+
+export default new Vuex.Store({state})
